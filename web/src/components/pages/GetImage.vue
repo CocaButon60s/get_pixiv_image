3c2480db63d115da3ff263b2966519d039f5f8f9
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2021-01-14 01:22:50 +0900

    画像取得処理をインターバルなしで実装

--- web/src/components/pages/GetImage.vue
+++ web/src/components/pages/GetImage.vue
@@ -19,19 +19,22 @@ export default {
         this.modal.showModal('IDエラー', '画像を取得するユーザーのIDを入力してください', false, {color: 'secondary', label: '閉じる'})
         return
       }
-      this.modal.showModal('画像取取得中', 'しばらくお待ちください', true, {color: 'danger', label: '中止', cb: this.cancel})
+      this.modal.showModal('画像取得中', '取得中', true, {color: 'danger', label: '中止', cb: this.cancel})
       // 画像取得
       await eel.py_set_target(this.id)()
-      var timer = setInterval(async () => {
-        var result = await eel.py_get_image()()
-        if (result === 'CONTINUE') return
-        if (result === 'SUCCESS') this.modal.showModal('画像取得成功', 'imagesフォルダ内を確認してください', false, {color: 'secondary', label: '閉じる'})
-        if (result === 'CANCEL') this.modal.showModal('画像取得中止', '画像取得を中止しました', false, {color: 'secondary', label: '閉じる'})
-        if (result === 'ERR') this.modal.showModal('画像取得失敗', 'IDを確認してください', false, {color: 'secondary', label: '閉じる'})
-        clearInterval(timer)
-      }, 2000)
+      this.get_image_loop()
     },
-    async cancel () { await eel.py_cancel()() }
+    async get_image_loop () {
+      var result = await eel.py_get_image()()
+      if (result === 'CONTINUE') setTimeout(this.get_image_loop, 0)
+      if (result === 'SUCCESS') this.modal.showModal('画像取得成功', 'imagesフォルダ内を確認してください', false, {color: 'secondary', label: '閉じる'})
+      if (result === 'CANCEL') this.modal.showModal('画像取得中止', '画像取得を中止しました', false, {color: 'secondary', label: '閉じる'})
+      if (result === 'ERR') this.modal.showModal('画像取得失敗', 'IDを確認してください', false, {color: 'secondary', label: '閉じる'})
+    },
+    async cancel () {
+      this.modal.showModal('画像取得中止中', '画像取得を中止します', true)
+      await eel.py_cancel()()
+    }
   },
   components: {InputForm, ButtonForm}
 }
